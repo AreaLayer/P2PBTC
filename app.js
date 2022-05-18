ce2126da369bc9ff6c973526caebf1f85b233904
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-05-18 08:21:35 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>P2P BTC</h1>
+      <p>Marketplace Global on Lightning Network powered by Layer2Labs</p>
+    </div>
+  );
+}
