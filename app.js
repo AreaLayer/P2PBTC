7029d8e1088a26eb4c18a666320895c762ef9404
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-05-25 07:55:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ export default function App() {
   return (
     <div>
       <h1>P2P BTC</h1>
-      <p>Marketplace Global on Lightning Network powered by Layer2Labs</p>
+      <p>Marketplace P2P Global on Lightning Network powered by Layer2Labs</p>
+
+      <h1>Coming soon</h1>
+      <p> This product is in development</p>
+
+      <h1>Features</h1>
+      <p>The Marketplace have security of the Layer 1 from Bitcoin;
+
+          For each good negotiation will be include a fee of 0.04%, 
+
+           Global;
+
+           Privacy with Onion (Tor).</p>
     </div>
   );
 }
