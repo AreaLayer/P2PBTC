be5e8ea012e839e650eb7456d175e800f31b8a38
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-12-05 09:42:59 -0300

    Update script.h

--- sricpt/script.h
+++ sricpt/script.h
@@ -7,8 +7,27 @@
 #ifndef BITCOIN_SCRIPT_SCRIPT_H
 #define BITCOIN_SCRIPT_SCRIPT_H
 
-# crypto
 
+#include <crypto/common.h>
+#include <prevector.h>
+#include <serialize.h>
+
+#include <assert.h>
+#include <climits>
+#include <limits>
+#include <stdexcept>
+#include <stdint.h>
+#include <string.h>
+#include <string>
+#include <uint256.h>
+#include <vector>
+
+enum opcodetype
+{
+
+// crypto
+  
 OP_RELALTIVETIME
 OP_NLOCKTIME
 OP_LOCKTIME
+  }
