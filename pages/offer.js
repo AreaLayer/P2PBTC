92a205b84dd270f40947c5b2ca8ce07301152b51
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-04-08 06:57:56 -0300

    Update offer.js

--- pages/offer.js
+++ pages/offer.js
@@ -1,3 +1,5 @@
 import lightning
 import bitcoin.js
-import lightning
+import lightning {
+  "https://github.com/alexbosworth/lightning/blob/master/lnd_gateway/lnd_gateway.js"
+  "https://github.com/alexbosworth/lightning/blob/master/lnd_gateway/gateway_request.js"
