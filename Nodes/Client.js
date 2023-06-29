47f1b834fd89eeb3b59e86427f2f2319e6e6c7ce
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-29 07:12:34 -0300

    Update Client.js

--- Nodes/Client.js
+++ Nodes/Client.js
@@ -1,2 +1,22 @@
-const lightning = require('lightning-js');
-const grpc = require('lightning-js');
+const { LightningClient } = require('./lnd_pb_service');
+const { GetInfoRequest } = require('./lnd_pb');
+
+const lndHost = 'http://localhost:8080'; // Replace with your LND host
+
+// Create the gRPC client
+const client = new LightningClient(lndHost, null, null);
+
+// Define a function to get LND node information
+async function getNodeInfo() {
+  const request = new GetInfoRequest();
+  try {
+    const response = await client.getInfo(request, {});
+    const info = response.toObject();
+    console.log('LND Node Info:', info);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+// Call the function to get node information
+getNodeInfo();
