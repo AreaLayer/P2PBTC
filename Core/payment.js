f91e59a0a799db0ceed0480b1a93003b86909ff9
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-19 09:22:23 -0300

    Update payment.js

--- Core/payment.js
+++ Core/payment.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { lnd } = require('ln-service');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/createInvoice', async (req, res) => {
+  try {
+    const { description, amount } = req.body;
+    const invoice = await lnd.createInvoice({ description, tokens: amount });
+    res.json({ invoice });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
 
