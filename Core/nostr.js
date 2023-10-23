79df8987c1322b89d76e4dcd345140ab77dbe66a
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-10-23 06:07:50 -0300

    Update nostr.js

--- Core/nostr.js
+++ Core/nostr.js
@@ -2,7 +2,7 @@ const NostrClient = require('nostr-js').NostrClient;
 const NostrUtils = require('nostr-js').NostrUtils;
 const WebSocket = require('ws');
 
-const websocketEndpoint = 'wss://damus.com'; // Replace with your Nostr server endpoint
+const websocketEndpoint = 'wss://relay.damus.com'; // Replace with your Nostr server endpoint
 const nostrClient = new NostrClient(new WebSocket(websocketEndpoint));
 
 const pubKey = 'your_pubkey_here'; // Replace with the PubKey you want to subscribe to
